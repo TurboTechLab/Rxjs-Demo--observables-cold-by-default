743b104d9bd04abced2587283863f79dc3212055
TurboTechLab <42316316+TurboTechLab@users.noreply.github.com>
2022-12-21 10:46:41 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -3,13 +3,13 @@ import { Observable } from 'rxjs';
 
 const randomValue = () => Math.round(Math.random() * 10);
 
-const coldByDefault = new Observable((observer) => {
+const unicastByDefault = new Observable((observer) => {
   observer.next(randomValue());
   observer.complete();
 });
 
 //Each subscription is likely to get different output
-coldByDefault.subscribe(v => console.log(v))
-coldByDefault.subscribe(v => console.log(v))
-coldByDefault.subscribe(v => console.log(v))
+unicastByDefault.subscribe(v => console.log(v))
+unicastByDefault.subscribe(v => console.log(v))
+unicastByDefault.subscribe(v => console.log(v))
 
